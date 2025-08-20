1412ebc1b3f8bb68202a3a100cf4b294fa139190
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2025-08-19 23:03:28 -0300

    Aula01/

---
+++ Aula01/exemplo_02.py
@@ -0,0 +1,13 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+# print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
+
+#numero = 3 # int
+#print(type(numero)) # Verifica o tipo da variável
+# numero_decimal = 4.78 # float
+# nome_de_usuario = "Eduardo" # string
+# verdadeiro = True # boolean
+
+nome = input("Digite seu nome: ")  # Solicita ao usuário que digite seu nome
+quantidade_de_caracteres = len(nome)  # Calcula o número de caracteres do nome
+print(quantidade_de_caracteres)

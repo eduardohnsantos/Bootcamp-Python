6a43782f13ef57ba2adccb5db1a66ed413336086
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2025-09-02 18:14:24 -0300

    Type hint

---
+++ Aula04/aula_algoritmo.py
@@ -0,0 +1,20 @@
+lista_numeros: list = [40, 50, 60, 70, 0, -408593, 1, 50]
+lista_numeros_02: list = [40, 50, 60, 70, 0, -408593, 1, 50]
+lista_numeros_03: list = [40, 50, 60, 70, 0, -408593, 1, 50]
+
+
+def ordenar_lista_numeros(numeros: list) -> list:
+    nova_lista_numeros: list = numeros.copy()
+
+    for i in range(len(nova_lista_numeros)):
+        for j in range(i + 1, len(nova_lista_numeros)):
+            if nova_lista_numeros[i] > nova_lista_numeros[j]:
+                nova_lista_numeros[i], nova_lista_numeros[j] = nova_lista_numeros[j], nova_lista_numeros[i]
+
+    return nova_lista_numeros
+
+nova_lista = ordenar_lista_numeros(lista_numeros)
+nova_lista_02 = ordenar_lista_numeros(lista_numeros_02)
+nova_lista_03 = ordenar_lista_numeros(lista_numeros_03)
+
+print(nova_lista)
---
+++ Aula04/data_science.py
@@ -0,0 +1,6 @@
+from aula_algoritmo import ordenar_lista_numeros
+
+lista = [3,5,10,-1,-3801]
+
+nova_lista = ordenar_lista_numeros(numeros=lista)
+print(nova_lista)

17ceb28037c96bd8f5837e32e3de4989cd0f0122
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2025-09-02 15:55:54 -0300

    Type hint

---
+++ Aula04/exemplo_dic.py
@@ -0,0 +1,25 @@
+lista = ["Sapato", 39, 10.38, True]
+
+produto_01: dict = {
+    "nome": "Sapato",
+    "quantidade": 39,
+    "preco": 10.38,
+    "disponibilidade": True
+
+}
+
+produto_02: dict = {
+    "nome": "Televisao",
+    "quantidade": 10,
+    "preco": 70.38,
+    "disponibilidade": False
+
+}
+
+carrinho: list = []
+
+carrinho.append(produto_01)
+carrinho.append(produto_02)
+
+print(carrinho)
+
